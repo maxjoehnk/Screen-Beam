1a7f1cd70b6fda9660fdfc9f168210b36e33a040
Max Jöhnk <maxjoehnk@gmail.com>
2024-10-04 20:01:46 +0200

    fix(server): unable to add new slide to screen without slides

--- server/src/db/queries/screens/insert_slide_into_screen.sql
+++ server/src/db/queries/screens/insert_slide_into_screen.sql
@@ -1,2 +1,2 @@
 insert into screen_slides (screen_id, slide_id, ordering)
-VALUES (?1, ?2, (select max(ordering) + 1 from screen_slides where screen_id = ?1));
+VALUES (?1, ?2, (select coalesce(max(ordering) + 1, 0) from screen_slides where screen_id = ?1));
